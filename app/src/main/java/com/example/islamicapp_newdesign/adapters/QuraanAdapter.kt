819957a2e816852095831fe4829fec2d510d74c7
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-02-07 10:25:30 +0200

    add : Quraan Design

---
+++ app/src/main/java/com/example/islamicapp_newdesign/adapters/QuraanAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.islamicapp_newdesign.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class QuraanAdapter :RecyclerView.Adapter<QuraanAdapter.ViewHolder>(){
+
+
+
+
+    class ViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
